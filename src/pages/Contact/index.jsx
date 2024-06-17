02710f1579bb7bdd5d81a785145acdf21eef33b4
Danil-Golubev <dokers190rus@mail.ru>
2024-06-17 23:31:34 +0300

    17-06-23:21

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -9,16 +9,24 @@ export const Contact = ()=>{
     
     return (
    <>
-    <div className={styles.container}>
-    <ItemContainerSkeleton/> 
-    <ItemContainerSkeleton/> 
-    <ItemContainerSkeleton/> 
-    <ItemContainerSkeleton/> 
-    <ItemContainerSkeleton/> 
-    <ItemContainerSkeleton/> 
-    <ItemContainerSkeleton/> 
-    <ItemContainerSkeleton/> 
-    </div>
+   <div className={styles.container}>
+<div className={styles.diagBlock}><div className={styles.diag}></div></div>
+<div className={styles.infoBlock}>
+<div className={styles.contentBlock}>
+<ul className={styles.contentUl}>
+<li className={styles.contentLi}><div className={styles.LiText}>Contact us</div></li>
+<li className={styles.contentLi}><div className={styles.LiText}>Have you checked our Customer Service pages to see if your inquiry is covered there?</div></li>
+<li className={styles.contentLi}><div className={styles.LiText}>Go to: </div></li>
+
+<li className={styles.contentLi}><input className = {styles.input}type='text' title     ={'enter the email here'}></input></li>
+<li className={styles.contentLi}><input className = {styles.input}type='text' title     ={'enter the email here'}></input></li>
+<li className={styles.contentLi}><input className = {styles.input}type='text' title     ={'enter the email here'}></input></li>
+</ul>
+
+</div>
+</div>
+</div>
+ 
    </>
     );
   };
