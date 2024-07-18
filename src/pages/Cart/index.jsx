a70381c10b2078a2d6957f794008eb098a9d99ac
Danil-Golubev <dokers190rus@mail.ru>
2024-07-18 21:24:54 +0300

    cart fix

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Button } from 'react-bootstrap';
 
 import styles from './Cart.module.scss';
 
 export const Cart = () => {
-	const storedObjects = JSON.parse(localStorage.getItem('cart') || '[]');
+	const [storedObjects, setStoredOjects] = React.useState(JSON.parse(localStorage.getItem('cart') || '[]'));
+	const [cart, setCart] = React.useState();
+	const cartChangeEvent = new Event('cartChange');
+	useEffect(() => {
+		setStoredOjects(JSON.parse(localStorage.getItem('cart') || '[]'));
+	}, [cart]);
+
 	const totalPrice = storedObjects
 		.map((item) => item.price * item.count)
 		.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 
-	function updateItemInLocalCache(targetId, targetSize) {
+	function updateItemInLocalCacheMinus(id, size) {
 		// Находим индекс нужного объекта
-		const index = storedObjects.findIndex((item) => item._id === targetId && item.selectedSize === targetSize);
+		const index = storedObjects.findIndex((item) => item._id === id && item.selectedSize === size);
 
 		if (storedObjects[index].count > 1) {
 			// Уменьшаем count на 1
@@ -23,41 +29,58 @@ export const Cart = () => {
 		}
 
 		// Сохраняем обновленные данные обратно в localStorage
-		localStorage.setItem('storedObjects', JSON.stringify(storedObjects));
-		console.log(targetId, targetSize);
+		localStorage.setItem('cart', JSON.stringify(storedObjects));
+		setStoredOjects(storedObjects);
+		setCart(storedObjects);
+
+		window.dispatchEvent(cartChangeEvent);
+	}
+	function updateItemInLocalCachePlus(id, size) {
+		// Находим индекс нужного объекта
+		const index = storedObjects.findIndex((item) => item._id === id && item.selectedSize === size);
+
+		storedObjects[index].count += 1;
+		// Сохраняем обновленные данные обратно в localStorage
+		localStorage.setItem('cart', JSON.stringify(storedObjects));
+		setStoredOjects(storedObjects);
+		setCart(storedObjects);
+
+		window.dispatchEvent(cartChangeEvent);
 	}
 
 	return (
 		<>
 			<div className={styles.content}>
 				<div className={styles.leftBlock}>
-					{storedObjects.map(
-						(item) => (
-							// eslint-disable-next-line react/jsx-key
-							<div className={styles.oneBlock}>
-								<img className={styles.imageBlock} src={item.imageUrl}></img>
+					{storedObjects?.map((item) => (
+						// eslint-disable-next-line react/jsx-key
+						<div className={styles.oneBlock}>
+							<img className={styles.imageBlock} src={item.imageUrl}></img>
 
-								<div className={styles.title}>
-									{item.title}
-									{`-${item.selectedSize}`}
-								</div>
-
-								<div className={styles.count}>
-									<Button
-										onClick={() => updateItemInLocalCache(item._id, item.selectedSize)}
-										className={styles.customButton}
-									>
-										{'-'}
-									</Button>
-									<div className={styles.customButtonCount}>{item.count}</div>
-									<Button className={styles.customButton}>{'+'}</Button>
-									<div></div>
-								</div>
-								<div className={styles.price}>${item.price * item.count}.00 </div>
+							<div className={styles.title}>
+								{item.title}
+								{`-${item.selectedSize}`}
 							</div>
-						),
-						[storedObjects],
-					)}
+
+							<div className={styles.count}>
+								<Button
+									className={styles.customButton}
+									onClick={() => updateItemInLocalCacheMinus(item._id, item.selectedSize)}
+								>
+									{'-'}
+								</Button>
+								<div className={styles.customButtonCount}>{item.count}</div>
+								<Button
+									className={styles.customButton}
+									onClick={() => updateItemInLocalCachePlus(item._id, item.selectedSize)}
+								>
+									{'+'}
+								</Button>
+								<div></div>
+							</div>
+							<div className={styles.price}>${item.price * item.count}.00 </div>
+						</div>
+					))}
 				</div>
 				<div className={styles.rightBlock}>
 					<div className={styles.rightBlockContent}>
